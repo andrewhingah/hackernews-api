c6d5bf1c5e063841bff8f6d6a239b896286054d4
Andrew Hinga <andrewhinga5@gmail.com>
2021-06-18 09:48:07 +0300

    add voting feature with realtime graphql subscriptions

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const Mutation = require("./resolvers/Mutation");
 const User = require("./resolvers/User");
 const Link = require("./resolvers/Link");
 const Subscription = require("./resolvers/Subscription");
+const Vote = require("./resolvers/Vote");
 const { getUserId } = require("./utils");
 
 const prisma = new PrismaClient();
@@ -18,6 +19,7 @@ const resolvers = {
   Subscription,
   User,
   Link,
+  Vote,
 };
 
 const server = new ApolloServer({
